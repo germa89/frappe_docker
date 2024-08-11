c84585ea24cb13f3e1280058502d3dfc7cffdfa7
Ger <28149841+germa89@users.noreply.github.com>
2024-08-11 11:34:42 +0200

    feat: using latest stable version

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 # set version
-export ERPNEXT_VERSION=v15.29.4
+export ERPNEXT_VERSION=v14.71.0
+export FRAPPE_VERSION=v14.79.0
 
 # build
-docker buildx bake --no-cache --set *.platform=linux/arm64 
+docker buildx bake --no-cache --set *.platform=linux/arm64 && \
 
 # Retagging
-docker tag frappe/erpnext:ger$ERPNEXT_VERSION  ghcr.io/germa89/erpnext:$ERPNEXT_VERSION
+docker tag frappe/erpnext:ger$ERPNEXT_VERSION  ghcr.io/germa89/erpnext:$ERPNEXT_VERSION && \
 
 # Pushing
 docker push ghcr.io/germa89/erpnext:$ERPNEXT_VERSION
